92ed7d1b7ea5f3541c5a716bdeb1956cc665fb2d
Michael Howard <x509cert@hotmail.com>
2024-02-15 21:30:58 -0600

    Create set-reset-tls.ps1
    PowerShell script to allow or block various TLS versions at the server

---
+++ set-reset-tls.ps1
@@ -0,0 +1,35 @@
+Set-StrictMode -Version Latest
+
+# Set to $true to enable TLS 1.0, 1.1 and 1.2
+$enableOldProtocols = $true
+
+$base = 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\'
+$tlsVersions = @('TLS 1.0', 'TLS 1.1')
+
+$enabledValue = if ($enableOldProtocols) { 1 } else { 0 }
+$disabledByDefaultValue = if ($enableOldProtocols) { 0 } else { 1 }
+
+foreach ($version in $tlsVersions) {
+    $path = $base + $version + '\Server'
+    New-Item $path -Force | Out-Null
+
+    # Create or set the 'Enabled' property
+    New-ItemProperty -Path $path `
+                     -Name 'Enabled' `
+                     -Value $enabledValue `
+                     -PropertyType 'DWord' `
+                     -Force | Out-Null
+                     
+    # Create or set the 'DisabledByDefault' property
+    New-ItemProperty -Path $path `
+                     -Name 'DisabledByDefault' `
+                     -Value $disabledByDefaultValue `
+                     -PropertyType 'DWord' `
+                     -Force | Out-Null
+    if ($enableOldProtocols) {
+        Write-Host "$version is Enabled."
+    } else {
+        Write-Host "$version is Disabled."
+    }
+}
+
